b1d5a79df093510ea7dc74d66e2db9b752873e0c
Wellington Souza <wellington031@gmail.com>
2018-10-06 15:06:52 -0300

    SQL HostGator

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -4,10 +4,10 @@ namespace Hcode\DB;
 
 class Sql {
 
-	const HOSTNAME = "127.0.0.1";
-	const USERNAME = "root";
-	const PASSWORD = "";
-	const DBNAME = "db_ecommerce";
+	const HOSTNAME = "162.241.2.202";
+	const USERNAME = "wellmark_user";
+	const PASSWORD = "AOTqU@}V8RF5";
+	const DBNAME = "wellmark_db";
 
 	private $conn;
 
